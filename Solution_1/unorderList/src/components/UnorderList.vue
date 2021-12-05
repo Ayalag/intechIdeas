7a09f097ee6db7a687f057b00efd933c52b8bd4f
Gadiel Ayala <gadiel.ayala@gmail.com>
2021-12-05 08:37:53 -0500

    Add files via upload

---
+++ Solution_1/unorderList/src/components/UnorderList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="list">
+    <ul>
+      <li v-for="(numbers, index) in 101" :key="index">
+        <div v-if="divisible(numbers + offset)">
+          {{ numbers + offset }}
+          <div class="result">{{ divisible(numbers + offset) }}</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UnorderList",
+data() {
+  return {
+    offset: 2
+  }
+},
+  methods: {
+    divisible(num) {
+      /* eslint-disable no-mixed-spaces-and-tabs */
+      if (num % 3 === 0 && num % 5 === 0) {
+        return "three and five";
+      }
+      if (num % 3 === 0) {
+        return "three";
+      }
+      if (num % 5 === 0) {
+        return "five";
+      }
+      return null;
+      /* eslint-enable no-mixed-spaces-and-tabs */
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+.list {
+  display: flex;
+  justify-content: center;
+}
+
+ul {
+  list-style-type: none;
+}
+
+li div {
+  border-radius: 5px;
+  font-size: 2rem;
+  display: flex;
+  justify-content: flex-end;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  background-color: rgb(163, 163, 163);
+  width: 33rem;
+  align-items: center;
+}
+</style>
